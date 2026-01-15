dd845917d594e97bff666885cb01774242951406
SeeleVollerei1810 <quann9076@gmail.com>
2026-01-15 14:11:36 +0700

    Update example results.py

--- Heat-Stress Metrics/example results.py
+++ Heat-Stress Metrics/example results.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import warnings
 
-file_path = '/content/drive/MyDrive/Group Project 2025/results/calculated_heatstress.nc'
-plot_existing_tw_analysis(file_path)
-
 print(fh.file_format)
 print(fh.dimensions.keys())
 print(fh.dimensions['time'])
@@ -92,3 +89,6 @@ def plot_existing_tw_analysis(file_path: str):
     plt.tight_layout()
     plt.show()
     ds.close()
+
+file_path = '/content/drive/MyDrive/Group Project 2025/results/calculated_heatstress.nc'
+plot_existing_tw_analysis(file_path)
